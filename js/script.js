4f3e3de8ba3ce1109da3df673af369a9bc7e7cca
Badsha023 <nmbadsha02@gmail.com>
2024-03-05 19:38:08 +0600

    start js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const postContainer = document.getElementById('posting');
+const cardRight = document.getElementById('second-card');
+const count = document.getElementById('counting');
+const cardArea = document.getElementById('card-container');
